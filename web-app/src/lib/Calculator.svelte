b36a32b4b3b64d29088e144371e7ec496914b2d6
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-06-24 17:10:05 +0800

    feat: calculator imports working.

---
+++ web-app/src/lib/Calculator.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Mania from '$lib/data/mania.json';
+    import Discoins from '$lib/data/discoin.json';
+
+    let initial_level = 1;              // Range: 1 - 89
+    let goal_level = 2;                 // Range: 2 - 90
+    let sinner = "";                    // For now, ignore.
+    let sinner_class = "b";
+    let refreshes = 0;                  // Range: 0 - 10
+    let black_key_available = false;
+    let phase_cost_yes = false;
+
+
+</script>
+
+<span>Initial Level:</span>
+<input bind:value={initial_level}/>
+
+<span>Goal Level:</span>
+<input bind:value={goal_level}/>
+
+<span>Sinner Class:</span>
+<input bind:value={refreshes}/>
+
+<h1>Discoins Need</h1>
+<h2>{Mania['s'][goal_level] - Mania['s'][initial_level]}</h2>
--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Calculator from '$lib/Calculator.svelte';
+</script>
+
+
+<Calculator></Calculator>
